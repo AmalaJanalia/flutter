62b01131b0852b2ac28bf53851a712726a6ed426
AmalaJanalia <amalajanalia223@gmail.com>
2023-06-29 11:18:41 +0530

    added text,changed font style,changed app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Amala Janalia APP"),
-          backgroundColor: Colors.yellow,
-        ),
-        body: const Center(
-          child: Image(image: AssetImage('images/flutterimage.jpeg')),
-        ),
-        backgroundColor: Colors.deepPurple[200],
-      ),
-    ),
-  );
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Image Viewer"),
+            backgroundColor: Colors.yellow,
+          ),
+          body: Column(children: [
+            const Image(image: AssetImage('images/flutterimage.jpeg')),
+            Container(
+              color: Colors.white60,
+              child: Text(
+                'Paris, the city of Lights, the fashion capital, the city of Love',
+                style: GoogleFonts.charm(
+                  textStyle:
+                      const TextStyle(fontSize: 20, color: Colors.deepPurple),
+                ),
+              ),
+            )
+          ]),
+        ));
+  }
 }
